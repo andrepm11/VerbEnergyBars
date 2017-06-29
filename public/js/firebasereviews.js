579cf5690fd97593955cb7ca06125b74fc759883
Andre Monteiro <andre.monteiro@yale.edu>
2017-06-29 00:28:35 -0700

    Transaction update

--- public/js/firebasereviews.js
+++ public/js/firebasereviews.js
@@ -24,8 +24,8 @@ $(document).ready(function () {
     });
     
     $('#review-form').submit(function(event) {
-       //event.preventDefault();
-	setTimeout(function () { window.location.reload(); }, 10);
+       event.preventDefault();
+//	   setTimeout(function () { window.location.reload(); }, 10);
 //        const mixpanelDistintctID = mixpanel.get_distinct_id();
         const mixpanelDistinctId = "12345";
 
@@ -34,15 +34,17 @@ $(document).ready(function () {
         const name = strip_html_tags(($("#js-name").val() == '') ? 'Anonymous' : $("#js-name").val());
         const comments = strip_html_tags($("#comments").val());
         const rating = parseInt($("input[name='rating']:checked").val());
-        const createdAt = (new Date().getTime())*-1;
+        
+        console.log(new Date().getTime());
 
-        const date = Date();
 
         firebase.auth().signInAnonymously().then(function(){
 
                 var database = firebase.database();
                 var usersRef = database.ref('/users');
-
+                var rootpath = database.ref();
+                var totalPath = database.ref('total_rating');
+            
                 usersRef.once('value', function(snapshot) {
                   if (snapshot.hasChild("/"+mixpanelDistinctId)) {
                       //If they've reviewed before
@@ -52,35 +54,45 @@ $(document).ready(function () {
                     }
                 });
 
-                database.ref("reviews").push({
-                    title,
-                    name,
-                    email,
-                    rating,
-                    comments,
-                    createdAt,
-                    date,
+                totalPath.transaction(function(total){
+
+                    if(total){
+                        console.log("worked");
+                        const date = Date();
+                        const createdAt = (new Date().getTime())*-1;
+                        
+                        database.ref("reviews").push({
+                            title,
+                            name,
+                            email,
+                            rating,
+                            comments,
+                            createdAt,
+                            date,
+                        });
+                        
+                        var updates={};
+
+                        rootpath.once("value").then(function(snapshot){
+                            updates[rating]=snapshot.val()[rating]+1;
+                            updates["num_ratings"] = snapshot.val().num_ratings+1;
+                            updates["total_rating"] = snapshot.val().total_rating+rating;
+                            updates["average_rating"] = updates["total_rating"] / updates["num_ratings"];
+                            rootpath.update(updates);
+                            return updates;
+                        })
+                        .catch(error=>{
+                            console.log(error)
+                        })
+                        
+                    }
+                    return total;
                 });
 
-                idsetter={};
-                idsetter[mixpanelDistinctId] = createdAt;
-                usersRef.set(idsetter);
+//                idsetter={};
+//                idsetter[mixpanelDistinctId] = createdAt;
+//                usersRef.set(idsetter);
 
-                var rootpath = database.ref();
-
-                var updates={};
-
-                rootpath.once("value").then(function(snapshot){
-                    updates[rating]=snapshot.val()[rating]+1;
-                    updates["num_ratings"] = snapshot.val().num_ratings+1;
-                    updates["total_rating"] = snapshot.val().total_rating+rating;
-                    updates["average_rating"] = updates["total_rating"] / updates["num_ratings"];
-                    rootpath.update(updates);
-                    return updates;
-                })
-                .catch(error=>{
-                    console.log(error)
-                })
         })
             .catch(error=>{
             console.log(error)
@@ -188,14 +200,13 @@ firebase.auth().signInAnonymously().then(function () {
         }
         var reviewsRef = database.ref('/reviews/');
         
-        reviewsRef.orderByChild("createdAt").limitToFirst(rowsPerPage).once("value", function(snapshot){
+        reviewsRef.orderByChild("createdAt").limitToFirst(rowsPerPage).on("value", function(snapshot){
             var i = 0;
             snapshot.forEach(function(data){
                 if(i==0){
                     stopAt=data.val().createdAt;
                 }
                 i+=1;
-                
                 var html='<div class="review">';
                 html+='<h3 class="review-title">' + data.val().title + '</h3>';
                 html+='<span class="review-stars">'
@@ -203,7 +214,7 @@ firebase.auth().signInAnonymously().then(function () {
                     html+='★';
                 }
                 html+='</span>';
-								html+='<span class="review-empty-stars">'
+                                html+='<span class="review-empty-stars">'
                 for(k=0;k<(5 - data.val().rating);k++){
                     html+='★';
                 }
@@ -211,13 +222,12 @@ firebase.auth().signInAnonymously().then(function () {
                 html+='<span class="reviewer-name">' + data.val().name + '</span>';
                 html+='<span class="review-date">' + data.val().date.substring(4,15)+'</span>';
                 html+='<p class="review-body">'+data.val().comments+'</p>';
-                
+
                 html+='</div>';
 
                 $("#reviewTable").append(html);
-
                 beginAt = data.val().createdAt;
-               //console.log(data.val());
+       
            }) 
         });
     });
